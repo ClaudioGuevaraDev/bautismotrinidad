544179adb50fdc3e2c74b48d36a3cf0b26b123df
Claudio Guevara <claudio.guevara@pharmkt.com>
2023-12-16 16:11:09 -0300

    Bajando tamaño de la letra

--- client/src/components/VideoSection.tsx
+++ client/src/components/VideoSection.tsx
@@ -108,7 +108,7 @@ function VideoSection({ images, messages, settings }: Props) {
               {index === text && (
                 <span
                   key={message._id}
-                  className="animate-fade animate-ease-in animate-duration-[2000ms] text-5xl font-extrabold leading-none tracking-tight text-white sm:text-6xl"
+                  className="animate-fade animate-ease-in animate-duration-[2000ms] text-xl font-extrabold leading-none tracking-tight text-white sm:text-3xl"
                 >
                   {message.text}
                 </span>
