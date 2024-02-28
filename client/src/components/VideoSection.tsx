0711ef14f0ca85233e2008aa0998361572cc845a
Claudio Guevara <claudio.guevara.dev@gmail.com>
2024-02-27 22:38:45 -0300

    quitando formulario

--- client/src/components/VideoSection.tsx
+++ client/src/components/VideoSection.tsx
@@ -1,7 +1,4 @@
-import "react-phone-input-2/lib/style.css";
-
 import { useEffect, useState } from "react";
-import PhoneInput from "react-phone-input-2";
 
 import { Image } from "../interfaces/images.interfaces";
 import { Message } from "../interfaces/messages.interfaces";
@@ -17,14 +14,13 @@ interface Props {
 function VideoSection({ images, messages, settings }: Props) {
   const [text, setText] = useState<number>(0);
   const [image, setImage] = useState<number>(0);
-  const [showForm, setShowForm] = useState<boolean>(false);
 
   useEffect(() => {
     if (!messages || !settings) return;
 
     const interval = setInterval(() => {
       if (text + 1 === messages.length) {
-        // setShowForm(true);
+        
       } else {
         setText(text + 1);
       }
@@ -53,57 +49,7 @@ function VideoSection({ images, messages, settings }: Props) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-8 h-[40rem]">
       <div className="text-center lg:text-left xl:shrink-0 h-full flex items-center">
-        {showForm ? (
-          <form className="animate-fade animate-ease-in animate-duration-[2000ms] p-4 bg-white rounded-lg shadow-md w-full max-w-lg mx-auto">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl mb-4">
-              Formulario para confirmar asistencia
-            </h1>
-            <div className="mb-4">
-              <label
-                htmlFor="fullName"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Nombre Completo
-              </label>
-              <input
-                type="text"
-                id="fullName"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                placeholder="Ingresa tu nombre completo"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Correo Electrónico
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                placeholder="Ingresa tu correo electrónico"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="fullName"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Número de Teléfono
-              </label>
-              <PhoneInput country="cl" inputProps={{ required: true }} />
-            </div>
-            <button
-              type="submit"
-              className="text-white bg-fuchsia-700 hover:bg-fuchsia-800 focus:ring-4 focus:outline-none focus:ring-fuchsia-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-            >
-              Aceptar Invitación
-            </button>
-          </form>
-        ) : (
-          messages?.map((message, index) => (
+        {messages?.map((message, index) => (
             <>
               {index === text && (
                 <span
@@ -114,8 +60,7 @@ function VideoSection({ images, messages, settings }: Props) {
                 </span>
               )}
             </>
-          ))
-        )}
+          ))}
       </div>
       {images && (
         <div className="max-w-md w-full mx-auto flex h-full items-center justify-center">
